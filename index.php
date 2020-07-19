821da59931fe6ea9262e400e6b73be47e50b84bd
rlaehdgns03 <rlaehdgns03@gmail.com>
2020-07-20 03:29:28 +0900

    great

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+     <link rel="stylesheet" type="text/css" href="http://localhost/style.css">
+</head>
+<body id="target">
+    <header>
+            <h1><a href="http://localhost/index.php">JavaScript</a></h1>
+    </header>
+    <nav>
+        <ol>
+            <?php
+               echo file_get_contents("list.txt");
+            ?>
+        </ol>  
+    </nav>
+    <div id="control">
+        <input type="button" value="white" onclick="document.getElementById('target').className='white'"/>
+        <input type="button" value="black" onclick="document.getElementById('target').className='black'"/>
+    </div>
+    <article>
+         <?php
+               if(empty($_GET['id']) == false){
+                    echo file_get_contents($_GET['id'].".txt");
+               }
+         ?>
+    </article>
+</body>
+</html>
