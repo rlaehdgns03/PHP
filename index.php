253f27ae1cd982c98acc68d1bd7f1d309233d2e9
rlaehdgns03 <rlaehdgns03@gmail.com>
2020-07-23 16:32:44 +0900

    veryvery

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <ol>
     <?php
         while($row=mysqli_fetch_assoc($result)){
-        echo '<li><a href="http://localhost/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
+        echo '<li><a href="http://localhost/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
         }
     ?>
     </ol>  
@@ -34,9 +34,9 @@
         $sql="SELECT topic.id,title,name,description FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id=".$_GET['id'];
         $result=mysqli_query($conn, $sql);
         $row=mysqli_fetch_assoc($result);
-        echo '<h2>'.$row['title'].'</h2>';
-        echo '<p>'.$row['name'].'</p>';
-        echo $row['description'];
+        echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
+        echo '<p>'.htmlspecialchars($row['name']).'</p>';
+        echo strip_tags($row['description']);
         }
     ?>
 </article>
