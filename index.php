5daf53e7b8c6a07e809478f905c5e10a6250e63b
rlaehdgns03 <rlaehdgns03@gmail.com>
2020-07-27 16:16:41 +0900

    LALALA

--- index.php
+++ index.php
@@ -11,15 +11,15 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="stylesheet" type="text/css" href="http://localhost/style.css">
-  <link href="http://localhost/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="/style.css">
+  <link href="/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body id="target">
   <div class="container">
  
     <header class="jumbotron text-center">
       <img src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" alt="생활코딩" class="img-circle" id="logo">
-      <h1><a href="http://localhost/index.php">JavaScript</a></h1>
+      <h1><a href="/index.php">JavaScript</a></h1>
     </header>
 
     <div class="row">
@@ -27,7 +27,7 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
         <ol class="nav nav-pills nav-stacked">
         <?php
         while( $row = mysqli_fetch_assoc($result)){
-          echo '<li><a href="http://localhost/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
+          echo '<li><a href="/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
         }
         ?>
         </ol>
@@ -53,12 +53,12 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
           <input type="button" value="white" onclick="document.getElementById('target').className='white'" class="btn btn-default  btn-lg"/>
           <input type="button" value="black" onclick="document.getElementById('target').className='black'" class="btn btn-default btn-lg"/>
         </div>
-        <a href="http://localhost/write.php" class="btn btn-success  btn-lg">쓰기</a>
+        <a href="/write.php" class="btn btn-success  btn-lg">쓰기</a>
       </div>
       </div>
     </div>
   </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script src="http://localhost/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+<script src="/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
 </body>
 </html>
